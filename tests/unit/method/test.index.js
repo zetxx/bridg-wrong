517a1d9454b429f6e4bbeb3b010915518ecb7f3a
zetxx <zetxx@belogradchik.biz>
2021-06-03 17:30:25 +0300

    fix: tests everywere

--- tests/unit/method/test.index.js
+++ tests/unit/method/test.index.js
@@ -1,11 +1,19 @@
 const tap = require('tap');
 const method = require('../../../lib/method')();
+const {NotFound} = require('../../../lib/method/errors');
 
 tap.test('Method', (t) => {
-    tap.type(method, 'object', 'method Is object');
-    tap.type(method.add, 'function', 'method.add is function');
-    tap.type(method.find, 'function', 'method.find is function');
-    tap.type(method.call, 'function', 'method.call is function');
+    t.type(method, 'object', 'method Is object');
+    t.type(method.add, 'function', 'method.add is function');
+    t.type(method.find, 'function', 'method.find is function');
+    t.type(method.call, 'function', 'method.call is function');
+    method.add('in.abc', () => 1);
+    method.add('in.async.abc', async() => 1);
+    t.throws(
+        () => method.find({direction: 'in', packet: {meta: {method: 'abcd'}}}),
+        new NotFound('method: abcd not found'),
+        'should throw'
+    );
     t.end();
 });
 
