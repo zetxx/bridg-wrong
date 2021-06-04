684cae68c6de95024d5f41e22bb93ce3f42d8c5d
zetxx <zetxx@belogradchik.biz>
2021-06-04 14:05:30 +0300

    fix: tests

--- tests/unit/method/test.index.js
+++ tests/unit/method/test.index.js
@@ -7,12 +7,40 @@ tap.test('Method', (t) => {
     t.type(method.add, 'function', 'method.add is function');
     t.type(method.find, 'function', 'method.find is function');
     t.type(method.call, 'function', 'method.call is function');
-    method.add('in.abc', () => 1);
-    method.add('in.async.abc', async() => 1);
+    method.add({method: 'abc.in', fn: () => 1});
+    
     t.throws(
         () => method.find({direction: 'in', packet: {meta: {method: 'abcd'}}}),
         new NotFound('method: abcd not found'),
         'should throw'
+    );
+
+    t.rejects(
+        method.call({direction: 'in', packet: {meta: {method: 'abcd'}}}),
+        new NotFound('method: abcd not found'),
+        'should throw'
+    );
+    t.throws(
+        () => method.find({direction: 'in', packet: {meta: {method: 'abcd'}}}),
+        new NotFound('method: abcd not found'),
+        'should throw'
+    );
+
+    t.type(
+        method.find({direction: 'in', packet: {meta: {method: 'abc'}}}),
+        'function',
+        'method.find should returns function'
+    );
+
+    t.resolves(
+        method.call({direction: 'in', packet: {meta: {method: 'abc'}}}),
+        'should resolves'
+    );
+
+    t.resolveMatch(
+        method.call({direction: 'in', packet: {meta: {method: 'abc'}}}),
+        1,
+        'should resolves to value'
     );
     t.end();
 });
