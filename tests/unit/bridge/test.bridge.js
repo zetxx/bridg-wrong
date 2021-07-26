b1309f67d409f771e5275e83a393832b10dae03c
zetxx <zetxx@belogradchik.biz>
2021-07-27 00:27:02 +0300

    fix: no power

--- tests/unit/bridge/test.bridge.js
+++ tests/unit/bridge/test.bridge.js
@@ -9,22 +9,57 @@ tap.test('Bridge', (l0) => {
     //     t.end();
     // });
 
-    l0.test('Method call positive cases', (t) => {
-        // set up
-        const bridgeA = new Bridge({config: {id: 'bridgeA'}});
-        const bridgeB = new Bridge({config: {id: 'bridgeB'}});
-        bridgeA.start({other: bridgeB});
-        bridgeB.start({other: bridgeA});
-        // register methods
-        bridgeA.methods.add({method: 'a.in', fn: ({payload}) => {
-            return payload;
-        }});
-        bridgeB.methods.add({method: 'a.out', fn: ({payload}) => {
-            return payload;
-        }});
+    l0.test('One Vector', (l1) => {
+        const bridge = new Bridge({config: {id: 'bridgeA', request: {waitTime: 5000}}});
+        bridge.method.add({
+            method: 'a.in',
+            fn: ({payload}) => payload + 1
+        });
+        bridge.method.add({
+            method: 'a.out',
+            fn: ({payload}) => payload + 43
+        });
+        // l1.test('outbound Direct Full fill after 100 ms', async(t) => {
+        //     let request = await bridge.pass({
+        //         packet: {
+        //             payload: 3,
+        //             meta: {method: 'a'}
+        //         },
+        //         direction: 'out'
+        //     });
+        //     setTimeout(bridge.request.fulfill(request), 100);
+        //     t.resolves(request.promise, 'should resolve');
+        //     t.end();
+        // });
 
-        bridgeA.pass({direction: 'in', packet: {meta: {method: 'a'}, payload: {a: 1}}});
-        t.end();
+        l1.test('outbound resolves when correct inbound received', async(t) => {
+            let requestOut = await bridge.pass({
+                packet: {
+                    payload: 3,
+                    meta: {method: 'a'}
+                },
+                direction: 'out'
+            });
+            setTimeout(async() => {
+                try {
+                    let requestIn = await bridge.pass({
+                        packet: {
+                            payload: 3,
+                            meta: {idx: requestOut.idx, nodeId: requestOut.nodeId}
+                        },
+                        direction: 'in'
+                    });
+                    await requestIn.promise;
+                } catch (e) {
+                    console.error(e);
+                }
+            }, 100);
+            await requestOut.promise;
+            t.end();
+        });
+
+        bridge.destroy();
+        l1.end();
     });
 
     l0.end();
