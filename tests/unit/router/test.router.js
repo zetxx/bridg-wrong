714c87d4f42502a24861a1f007fa61f1ceae79a6
zetxx <zetxx@belogradchik.biz>
2022-06-07 16:16:51 +0300

    fix: arg

--- tests/unit/router/test.router.js
+++ tests/unit/router/test.router.js
@@ -10,26 +10,33 @@ const {
 const log = (level, msg) => console[level](msg)
 
 tap.test('Router', (l0) => {
+    const v1 = vectorFactory({
+        log,
+        config: {
+            request: {waitTime: 5000},
+            id: 'v1'
+        }
+    });
+    const v2 = vectorFactory({
+        log,
+        config: {
+            request: {waitTime: 10000},
+            id: 'v2'
+        }
+    });
+    methodRegisterFactory(v1, 'a.in');
+    methodRegisterFactory(v1, 'a.out');
+    methodRegisterFactory(v2, 'a.in');
+    methodRegisterFactory(v2, 'a.out');
+
     const router = Router({
         log,
-        vectors: [vectorFactory({
-            log,
-            config: {
-                request: {waitTime: 5000},
-                id: 'v1'
-            }
-        }), vectorFactory({
-            log,
-            config: {
-                request: {waitTime: 10000},
-                id: 'v2'
-            }
-        })]
+        vectors: [v1, v2]
     });
 
     router.pass({
         vector: 0,
-        packet: 
+        packet: {}
     })
 
     l0.end();
