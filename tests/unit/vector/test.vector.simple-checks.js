9fb47920bef43c84b09c85b4affd348402756c0d
zetxx <zetxx@belogradchik.biz>
2022-07-13 00:06:49 +0300

    fix: tests simple

--- tests/unit/vector/test.vector.simple-checks.js
+++ tests/unit/vector/test.vector.simple-checks.js
@@ -1,12 +1,15 @@
 const tap = require('tap');
+const Vector = require('../../../lib/vector');
 const {
     timeOut,
     vectorFactory,
     methodRegisterFactory
 } = require('../helpers');
 
-tap.test('Vector: Simple checks and coverage', (l0) => {
-    const v1 = vectorFactory({config: {id: 'V'}});
+tap.test('Vector: Simple checks and coverage', async(l0) => {
+    Vector();
+    const v1 = vectorFactory({config: {id: 'V', request: {waitTime: 3000}}});
+    l0.resolves(v1.start(), 'start should resolves');
     v1.destroy();
     l0.end();
 });
