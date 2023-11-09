93e53656f7c1aa0f6c75d6dc5a7ef8af451b735c
Elin Angelow <elin.angelov@shelly.com>
2023-11-09 16:41:36 +0200

    docs

--- tests/unit/index.js
+++ tests/unit/index.js
@@ -1,5 +1,5 @@
-// require('./test.methods');
-// require('./test.packets');
+require('./test.methods');
+require('./test.packets');
 require('./wire/test.wire.simple');
 // @TODO wire timeouts
 // require('./router/test.router');
