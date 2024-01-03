a1124271adc0583566498626e9a8cbee1aeb6df1
Elin Angelow <elin.angelov@shelly.com>
2024-01-03 11:49:22 +0200

    fix: tests

--- tests/unit/test.methods-wires.js
+++ tests/unit/test.methods-wires.js
@@ -3,17 +3,29 @@ const Methods = require('../../lib/methods');
 const Wires = require('../../lib/wires');
 const wires = Wires({});
 let requests = new Map();
+new (Methods({
+    wires
+})); // coverage
 const methods1 = new (Methods({
     wires,
     list: requests,
-    config: {timeout: 1000 * 60 * 100, namespace: 'testmethods'}
+    config: {namespace: 'testmethods'}
 }));
 const methods2 = new (Methods({
     wires,
     list: requests,
-    config: {timeout: 1000 * 60 * 100, namespace: 'testmethods12'}
+    config: {namespace: 'testmethods12'}
+}));
+const methods3 = new (Methods({
+    wires,
+    list: requests,
+    config: {namespace: 'ethods12'}
 }));
 methods2.add({name: 'a.b.fn1', fn: (m) => 123});
+methods2.add({name: 'a.b.fn.ask', fn: async(m, {ask}) => (await ask({method: 'a.b.fn3', meta: {timeout: 1000 * 30}})).params});
+methods2.add({name: 'a.b.fn.notify', fn: async(m, {notify}) => await notify({method: 'a.b.fn3'})});
+methods3.add({name: 'a.b.fn3', fn: (m) => 123});
+methods3.add({name: 'a.b.fn2.throws', fn: (m) => {throw new Error('abc')}});
 tap.test('Wires + Methods', async(t) => {
     try {
         await methods1.ask({id: 1, method: 'a1', params: [123]});
@@ -45,6 +57,27 @@ tap.test('Wires + Methods', async(t) => {
         true,
         'check response of notification'
     );
+    r = await methods1.ask({method: 'a.b.fn.ask', params: [123], meta: {timeout: 1000 * 30}});
+    t.same(
+        r.params,
+        123,
+        'check response'
+    );
+    r = await methods1.notify({method: 'a.b.fn.notify', params: [123]});
+    t.same(
+        r,
+        true,
+        'check notify response'
+    );
+    try {
+        r = await methods1.ask({method: 'a.b.fn2.throws', params: [123]});
+    } catch ({error}) {
+        t.same(
+            error.message,
+            'abc',
+            'check throw in method'
+        );
+    }
     t.end();
 });
 
